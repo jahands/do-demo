54862b7f1a6f486a3c83e17c0208de761315c1a8
Jacob Hands <jacob@gogit.io>
2023-02-01 05:39:38 -0600

    Stop overengineering it

--- src/TextSync.ts
+++ src/TextSync.ts
@@ -1,34 +1,26 @@
 import { Env } from "./types";
 
-interface TextSyncPair {
-  value: string
-  consumed: boolean
-}
-interface TextSyncPairs {
-  [key: string]: TextSyncPair
-}
-
 export class TextSync {
   state: DurableObjectState
   env: Env
-  pairs: TextSyncPairs
+  text: string | undefined
   constructor(state: DurableObjectState, env: Env) {
     this.state = state
     this.env = env
-    this.pairs = {}
   }
 
   async fetch(request: Request) {
-    if(request.method === 'POST') {
-      const text = await request.text()
-      this.pairs[request.url] = { value: text, consumed: false }
-      return new Response('The text was consumed!')
-    } else if (request.method === 'GET') {
+    if (request.method === 'GET') {
       // Wait for the sender before returning
-      while (!this.pairs[request.url]) {
+      while (!this.text) {
         await sleep(100)
       }
-      return new Response(this.pairs[request.url].value)
+      return new Response(this.text)
+
+    }else if(request.method === 'POST') {
+      const text = await request.text()
+      this.text = text
+      return new Response('The text was consumed!')
     }
   }
 }
